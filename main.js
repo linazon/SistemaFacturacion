d6467c2d51fb8a4bc861996ff5822855993d6eba
Santiago Mejia <santiagomejiad19@hotmail.com>
2021-05-31 18:31:22 -0500

    Add Inquirer and GitIgnore

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ const inquirer = require("inquirer");
 
 let Electrodomesticos = require('./Electrodomesticos');
 let valor = new Electrodomesticos('Nacional', "A");
-console.log(valor);
+//console.log(valor);
 
 let Televisor = require('./Televisor');
 let valor2 = new Televisor('Nacional', "C", 50, true);
-console.log(valor2, valor2.calcularPrecioBase());
+//console.log(valor2, valor2.calcularPrecioBase());
 
 let Nevera = require('./Nevera');
 let valor3 = new Nevera('Internacional', "B", 300);
-console.log(valor3, valor3.calcularPrecioBase());
+//console.log(valor3, valor3.calcularPrecioBase());
 
 /*
 console.log("Seleccione una opcion\n"+
@@ -22,6 +22,13 @@ let opcElectr = "texto";
 console.log("¿Cuántos electrodomesticos va a ingresar?");
 let contElectr = 5;
 */
+let arregloElectro = [];
+let agregar = require('./Inventario');
+//agregar(Electrodomesticos, "A", "Importado",3,arregloElectro);
+//console.log(arregloElectro)
+
+
+// ejemplo para 
 const obtenerConsumo = async (msg) => {
     const { consumo } = await inquirer.prompt({
       type: "input",
@@ -30,10 +37,37 @@ const obtenerConsumo = async (msg) => {
     });
     return consumo;
   };
-  
+    
+  const cargarInventario= async() => {
+    let cantidadElectrodomestico = await obtenerConsumo("Ingrese la cantidad de electrodomesticos a agregar");
+    let procedencia = await obtenerConsumo("ingresa la procedencia\n 1. Internacional 0.Nacional");
+    let tipoElectrodimestico = await obtenerConsumo("ingresa el tipo de electrodomestico \n 1. Electrodomestico general\n 2. Nevera\n 3.Televisor");
+    
+    let consumo;
+    var ciclo = true;
+    while (ciclo){
+      cantidadElectrodomestico = cantidadElectrodomestico *1;
+      consumo = await obtenerConsumo("Seleccione un consumo○\n A, B, C");
+      switch(consumo.toUpperCase()){
+        case "A":  
+        case "B":  
+        case "C": return ciclo = false; 
+        default:
+          console.log("Ingrese la opcion correcta");
+          continue;
+      }
+   
+    }
+    console.log(consumo);
+  }
+
   const main = async () => {
-    const resultado = await obtenerConsumo("Cual es el consumo del electrodomestico");
-    console.log(resultado);
+    //const resultado = await obtenerConsumo("Cual es el consumo del electrodomestico");
+    //const procedencia = await obtenerConsumo("ingresa la procedencia");
+    cargarInventario();
+    agregar(Electrodomesticos, "A", "Importado",3,arregloElectro);
+   // console.log(arregloElectro) 
+   // console.log(resultado + "prueba" + procedencia);
   };
   
   main();
